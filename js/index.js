0b4e26843cb8fea5d8e46fd954643a15ad329d4c
CornerstoneII <seunmyles@gmail.com>
2020-09-29 04:16:52 +0100

    Fix Review Changes

--- js/index.js
+++ js/index.js
@@ -5,4 +5,49 @@ $(document).ready(function() {
         return $('#popover-content').html();
       }
     });
+
+    $(".dropdown-menu li a").click(function(){
+      var selText = $(this).text();
+      $(this).parents('.input-group-btn').find('.bs-dropdown-to-select').html('<span><i class="fa fa-caret-down" aria-hidden="true"></i></span> '+selText).dropdown('toggle');
+      return false;
+    });
+
+    $('.slider').slick({
+      centerMode: true,
+      centerPadding: '20px',
+      infinite: true,
+      arrows: true,
+      speed: 250,
+      dots: false,
+      responsive: [
+          {
+          breakpoint: 1440,
+          settings: {
+              slidesToShow: 4,
+          }
+          },
+          {
+          breakpoint: 769,
+          settings: {
+              slidesToShow: 4,
+              centerPadding: '10px',
+          }
+          },
+          {
+          breakpoint: 703,
+          settings: {
+              slidesToShow: 4,
+              centerPadding: '10px',
+          }
+          },
+          {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 2,
+              centerMode: false,
+              arrows: false
+          }
+          }
+      ]
   });
+});
